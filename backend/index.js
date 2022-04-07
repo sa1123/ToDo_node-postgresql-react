edf2d89ffed899401a98edee75876388144cabd9
sa1123 <sahil2398@yahoo.com>
2022-04-08 01:26:16 +0530

    fetching data from db

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+const knex = require('knex'); //for sql
+
+require('dotenv').config({ path: './.env'});
+
+// console.log(process.env)
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE,
+    },
+});
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Home Route'));
+app.get('/', (req, res) => {
+    db.select('*')
+        .from('todo')
+        .then((data) => {
+            console.log(data);
+            res.json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
 const port = process.env.PORT || 5000;
 
